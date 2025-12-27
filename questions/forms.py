1d6b518973acea5e0f7fc0837b3fc4c4d1700784
DavidOhlendorf <ohlendorf@dzhw.eu>
2025-12-27 13:14:28 +0100

    added two form fields on question edit page

--- questions/forms.py
+++ questions/forms.py
@@ -6,12 +6,14 @@ class QuestionEditForm(forms.ModelForm):
 
     class Meta:
         model = Question
-        fields = ["questiontext", "question_type", "instruction", "item_stem"]
+        fields = ["questiontext", "question_type", "instruction", "item_stem", "missing_values", "top_categories"]
         widgets = {
             "questiontext": forms.Textarea(attrs={"rows": 4, "class": "form-control"}),
             "question_type": forms.Select(attrs={"class": "form-select"}),
             "instruction": forms.Textarea(attrs={"rows": 3, "class": "form-control"}),
             "item_stem": forms.Textarea(attrs={"rows": 3, "class": "form-control"}),
+            "missing_values": forms.Textarea(attrs={"rows": 3, "class": "form-control"}),
+            "top_categories": forms.Textarea(attrs={"rows": 3, "class": "form-control"}),
         }
 
     def __init__(self, *args, **kwargs):
