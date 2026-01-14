647899e91a3c1f9125e4eda176b3f1017f5ffe05
DavidOhlendorf <ohlendorf@dzhw.eu>
2026-01-14 13:22:49 +0100

    added URLs for page duplicator

--- pages/urls.py
+++ pages/urls.py
@@ -15,10 +15,20 @@ urlpatterns = [
     # 2 POST-Endpunkte für die Bearbeitung von Basis- und Inhaltsdaten einer Seite
     path("<int:pk>/edit/base/", views.WavePageBaseUpdateView.as_view(), name="page-edit-base"),
     path("<int:pk>/edit/content/", views.WavePageContentUpdateView.as_view(), name="page-edit-content"),
+
     # Ein POST-Endpunkt zum Löschen einer Seite
     path("<int:pk>/delete/", views.WavePageDeleteView.as_view(), name="page-delete"),
+
     # Programmiervorlage (PV) einer Seite anzeigen
     path("<int:pk>/pv/", views.WavePagePVView.as_view(), name="pv"),
+
+    # API für Modal zum Duplizieren von Seiten
+    path("api/surveys/", views.SurveyListApiView.as_view(), name="api-surveys"),
+    path("api/surveys/<int:survey_id>/waves/", views.WavesBySurveyApiView.as_view(), name="api-waves-by-survey"),
+    path("api/pages/check-name/", views.CheckPageNameApiView.as_view(), name="api-check-page-name"),
+
+    # Seite duplizieren
+    path("<int:pk>/copy/", views.WavePageCopyView.as_view(), name="page-copy"),
 
 ]
  
