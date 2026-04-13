5c1414a6162f1084f0f9089eb038b5c1cfccbc43
DavidOhlendorf <ohlendorf@dzhw.eu>
2026-04-13 17:33:53 +0200

    Fix WaveDocument storage migration drift

--- waves/storage.py
+++ waves/storage.py
@@ -1,7 +1,13 @@
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from django.utils.deconstruct import deconstructible
 
 
-wave_document_storage = FileSystemStorage(
-    location=settings.PRIVATE_UPLOAD_ROOT / "wave_documents"
-)
+@deconstructible
+class WaveDocumentStorage(FileSystemStorage):
+    def __init__(self, *args, **kwargs):
+        kwargs["location"] = settings.PRIVATE_UPLOAD_ROOT / "wave_documents"
+        super().__init__(*args, **kwargs)
+
+
+wave_document_storage = WaveDocumentStorage()
