5f26772a0502cff8daa9f276c41567d1936ce257
DavidOhlendorf <ohlendorf@dzhw.eu>
2026-07-22 14:42:28 +0200

    added QuestionVersionGroup in admin area

--- questions/admin.py
+++ questions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin 
 from import_export.admin import ImportExportModelAdmin
-from .models import Question, Keyword, Construct, ConstructPaper
+
+from django.db import Count
+
+from .models import Question, Keyword, Construct, ConstructPaper, QuestionVersionGroup
 from .resources import QuestionResource, KeywordResource, ConstructResource, ConstructPaperResource
 from waves.models import WaveQuestion
 
@@ -8,11 +11,41 @@ class WaveQuestionInline(admin.TabularInline):
     model = WaveQuestion
     extra = 0
 
+@admin.register(QuestionVersionGroup)
+class QuestionVersionGroupAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "question_count", "created_at")
+    search_fields = ("name", "note")
+    readonly_fields = ("created_at",)
+    ordering = ("name", "id")
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            _question_count=Count("questions")
+        )
+
+    @admin.display(description="Fragen", ordering="_question_count")
+    def question_count(self, obj):
+        return obj._question_count
+
+
+
 @admin.register(Question)
 class QuestionAdmin(ImportExportModelAdmin):
     resource_class = QuestionResource
-    list_display = ("id", "legacy_id", "questiontext",)
-    search_fields = ('questiontext',)
+    list_display = (
+        "id",
+        "legacy_id",
+        "questiontext",
+        "version_group",
+        "version_number",
+    )
+    search_fields = (
+        "questiontext",
+        "version_group__name",
+    )
+    list_filter = ("version_group",)
+    list_select_related = ("version_group",)
+    autocomplete_fields = ("version_group",)
     inlines = [WaveQuestionInline]
 
 
